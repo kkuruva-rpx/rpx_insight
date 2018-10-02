759757b29ecdc8330958ef9b8e77a48096ca7454
Keshav Prasad Kuruva <kkuruva_c@rpxcorp.com>
2018-10-02 11:06:32 -0700

    Added bootstrap

--- insight-ng/src/app/app.module.ts
+++ insight-ng/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InsightApiService} from './insight-api.service'l
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [InsightApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- insight-ng/src/app/insight-api.service.ts
+++ insight-ng/src/app/insight-api.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class InsightApiService {
 
+  INSIGHT_API_URL: string = "http://localhost:3000";
+
   constructor() { }
 
 }
