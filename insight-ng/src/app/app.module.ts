69fd242f33f1d634e2f5664ab109c2fa5e850583
Keshav Prasad Kuruva <kkuruva_c@rpxcorp.com>
2018-10-02 12:52:57 -0700

    Created Bootstrap | Layouts | Header/Footer/Sidebar/Dashboard and Shared component

--- insight-ng/src/app/app.module.ts
+++ insight-ng/src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { InsightApiService} from './insight-api.service'l
+import { DashboardModule } from './dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InsightApiService } from './insight-api.service';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    rootRouting,
+    DashboardModule,
+    SharedModule
   ],
   providers: [InsightApiService],
   bootstrap: [AppComponent]
