7c91161410f80504357608f365dc6166a7c47216
Keshav Prasad Kuruva <kkuruva_c@rpxcorp.com>
2018-10-02 12:51:52 -0700

    Created Bootstrap | Layouts | Header/Footer/Sidebar/Dashboard and Shared component

---
+++ insight-ng/src/app/shared/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'layout-footer',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
